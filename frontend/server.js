0c41bca1068bc43987601a8eb868b37a94d36846
MoosaTae <sadhis.tae@gmail.com>
2023-09-07 16:02:19 +0700

    test

--- frontend/server.js
+++ frontend/server.js
@@ -4,7 +4,7 @@ const app = express();
 
 app.use(express.static("public"));
 
-const PORT = 2222;
+const PORT = 3333;
 app.listen(PORT, "0.0.0.0", () => {
   console.log(`Frontend Server ready at http://localhost:${PORT}`);
 });
