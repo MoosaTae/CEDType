ba8b968ababac3a5c29fd02ca99486c43d8cf588
MoosaTae <sadhis.tae@gmail.com>
2023-09-27 22:12:29 +0700

    v.0.0.1 merged tutorial to main

--- frontend/public/scripts/typing.js
+++ frontend/public/scripts/typing.js
@@ -224,3 +224,27 @@ function botPractice() {
     removeClass(document.getElementById('game'), 'over');
     document.getElementById('botRow').style.marginTop = 0;
 }
+import { loadParagraph, initTyping, resetGame } from "./state.js";
+import { initializeButton } from "./option.js";
+
+const inpField = document.querySelector("#input-container");
+const tryAgainBtn = document.querySelector("#restart-button");
+
+function gameStart() {
+    initializeButton();
+    loadParagraph();
+
+    inpField.addEventListener("input", initTyping);
+    tryAgainBtn.addEventListener("click", resetGame);
+    inpField.addEventListener("keydown", (e) => {
+    if (e.keyCode == 9) {
+        resetGame();
+    }
+    });
+    tryAgainBtn.addEventListener("keydown", (e) => {
+    if (e.keyCode == 9) {
+        resetGame();
+    }
+    });
+}
+export {gameStart};
