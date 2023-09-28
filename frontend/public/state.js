389065f0af0e9524b2ed3061adfb94c2b1e50517
goffu7 <135923234+goffu7@users.noreply.github.com>
2023-09-28 20:29:41 +0700

    merge js from main
    missing script from main

---
+++ frontend/public/state.js
@@ -0,0 +1,159 @@
+import { words } from "./items.js";
+
+const typingText = document.querySelector(".typing-text");
+const inpField = document.querySelector("#input-container");
+const timeTag = document.querySelector("#time");
+//const mistakeTag = document.querySelector(".mistake span")
+const wpmTag = document.querySelector("#wpm");
+const scoreTag = document.querySelector("#score");
+const timeFrequency = 10; // millisecond
+
+let timer;
+let maxTime = 60;
+let timeLeft = maxTime;
+let charIndex = 0;
+let mistakes = 0
+let isTyping = 0;
+let misplay = 0;
+let score = 0;
+let multiplier = 1;
+let timepass = 0;
+let max_length = 10;
+let min_length = 5;
+let game_mode = "endless-mode";
+
+function generateRandomParagraph() {
+  const paragraphLength = Math.floor(Math.random() * (max_length - min_length + 1)) + min_length;
+  const paragraph = [];
+  
+  for (let i = 0; i < paragraphLength; i++) {
+      const randomWordIndex = Math.floor(Math.random() *words.length);
+      paragraph.push(words[randomWordIndex]);
+  }
+  return paragraph.join(' ');
+}
+
+function addParagraph() {
+  if (game_mode == "endless-mode") {
+    let charactersIndex = Math.floor(Math.random() * words.length);
+    words[charactersIndex].split("").forEach((char) => {
+      let span = `<span>${char}</span>`;
+      typingText.innerHTML += span;
+    });
+  }
+  else if (game_mode == "time-mode") {
+    let paragraph = generateRandomParagraph();
+    for (let i = 0; i < paragraph.length; i++) {
+      let span = `<span>${paragraph[i]}</span>`;
+      typingText.innerHTML += span;
+    }
+  }
+  // typingText.querySelector("span").classList.add("active");
+}
+function resetWord(){
+  typingText.innerHTML = "";
+  inpField.value = "";
+  charIndex = 0;
+  addParagraph();
+}
+
+// randomly load a paragraph from the words
+function loadParagraph() {
+  resetWord()
+  document.addEventListener("keydown", () => inpField.focus());
+  typingText.addEventListener("click", () => inpField.focus());
+}
+
+function resetGame() {
+  loadParagraph();
+  clearInterval(timer);
+  timeLeft = maxTime;
+  charIndex = mistakes = isTyping = 0;
+  inpField.value = "";
+  timeTag.innerText = timeLeft;
+  wpmTag.innerText = 0;
+  scoreTag.innerText = 0;
+  score = 0;
+  multiplier = 1;
+  timepass = 0;
+}
+
+function initTyping() {
+  let characters = typingText.querySelectorAll("span");
+  let typedChar = inpField.value.split("")[charIndex];
+  if (timeLeft > 0) {
+    // start timer
+    if (!isTyping) {
+      timer = setInterval(initTimer, timeFrequency);
+      isTyping = true;
+    }
+    // delete case
+    if (typedChar == null) {
+      // is it start of the paragraph?
+      if (charIndex > 0) {
+        charIndex--;
+        if (characters[charIndex].classList.contains("incorrect")) {
+          misplay = 0;
+        }
+        characters[charIndex].classList.remove("correct", "incorrect");
+      }
+    }
+    // normal case
+    else {
+      if (characters[charIndex].innerText == typedChar) {
+        characters[charIndex].classList.add("correct");
+      } 
+      else {
+        misplay = 1;
+        characters[charIndex].classList.add("incorrect");
+      }
+      charIndex++;
+      if (charIndex == characters.length) {
+        CheckWord();
+      }
+    }
+  } 
+  else {
+    clearInterval(timer);
+    inpField.value = "";
+  }
+}
+
+function CheckWord() {
+  if (misplay == 0) {
+    score++;
+    scoreTag.innerText = score;
+    timeLeft += 2.5;
+  }
+  else {
+    timeLeft -= 5;
+  }
+  resetWord();
+}
+
+function initTimer() {
+  if (timeLeft > 0) {
+    timeLeft -= 0.01 * multiplier;
+    timepass += 0.01;
+    multiplier = Math.ceil(timepass / 30);
+    if (timeLeft < 0) timeLeft = 0;
+    timeTag.innerText = timeLeft;
+    let wpm = Math.round((score * 60) / timepass);
+    wpmTag.innerText = wpm;
+  }
+  else {
+    clearInterval(timer);
+  }
+}
+
+function setMaxTime(newTime) {
+  maxTime = newTime;
+  resetGame();
+}
+
+function changeMode(mode){
+  game_mode = mode;
+  resetGame();
+}
+
+export { loadParagraph, initTyping , resetWord, resetGame, maxTime, setMaxTime, initTimer, changeMode }
