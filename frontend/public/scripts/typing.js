91b6cefdde609b45ace357b923d5fc45ae39dedd
MoosaTae <sadhis.tae@gmail.com>
2023-10-11 11:57:15 +0700

    solve reset when tab

--- frontend/public/scripts/typing.js
+++ frontend/public/scripts/typing.js
@@ -4,23 +4,22 @@ import { InitLeaderboard } from './leaderboard.js'
 
 const inpField = document.querySelector('#input-container')
 const tryAgainBtn = document.querySelector('#restart-button')
-
+const timeGame = document.querySelector('#timegame')
+function TabReset(e) {
+  if (e.code == 'Tab') {
+    e.preventDefault() // Prevent the default tab key behavior
+    resetGame()
+  }
+}
 function gameStart() {
-    initializeButton()
-    InitLeaderboard()
-    loadParagraph()
+  initializeButton()
+  InitLeaderboard()
+  loadParagraph()
+  inpField.addEventListener('keydown', TabReset)
+  tryAgainBtn.addEventListener('keydown', TabReset)
+  timeGame.addEventListener('keydown', TabReset)
 
-    inpField.addEventListener('input', initTyping)
-    tryAgainBtn.addEventListener('click', resetGame)
-    inpField.addEventListener('keydown', (e) => {
-        if (e.keyCode == 9) {
-            resetGame()
-        }
-    })
-    tryAgainBtn.addEventListener('keydown', (e) => {
-        if (e.keyCode == 9) {
-            resetGame()
-        }
-    })
+  inpField.addEventListener('input', initTyping)
+  tryAgainBtn.addEventListener('click', resetGame)
 }
 export { gameStart }
